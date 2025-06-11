0d8f7d3c855aced86c2e673a5f2d2b5322feed6e
Gary Johnson <gary@gjtt.com>
2025-06-10 20:33:16 -0400

    WIP

--- install/hammer-editor-install.sh
+++ install/hammer-editor-install.sh
@@ -14,7 +14,7 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y unzip curl
+$STD apt-get install -y unzip curl openjdk-21-jre
 msg_ok "Installed Dependencies"
 
 msg_info "Installing hammer-editor"
